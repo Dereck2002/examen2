f84c4ce0b26c36f7110bc0b10b3ceda935509938
Dereck2002 <jaramilloderek13@gmail.com>
2023-06-30 17:34:54 -0500

    .

--- vistas/home.php
+++ vistas/home.php
@@ -1,5 +1,5 @@
 <div class="container is-fluid">
     <h1 class="title" >Home</h1>
-    <h2 class="subtitle" >Bienvenido <?php echo $_SESSION['nombres']." ". $_SESSION['apellidos']?></h2>
+    <h2 class="subtitle" >Bienvenido <?php echo $_SESSION['nombres']." "?></h2>
 
 </div>
